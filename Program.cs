1e304b73d9f87518934883eb1bea8ec0a5ff5a5a
Jesse White <jesse.white6@gmail.com>
2021-05-03 13:36:33 -0700

    initial file creation and co-author.
    Co-authored-by: Thomas Russell <t.p.russell19@gmail.com>

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace riddles_of_the_sphinx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
