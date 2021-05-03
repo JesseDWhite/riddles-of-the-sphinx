c8c2b522bddcf856626777ae21f8035384afe777
Jesse White <jesse.white6@gmail.com>
2021-05-03 15:30:29 -0700

    complete random questions
    Co-authored-by: Thomas Russell <t.p.russell19@gmail.com>

--- Program.cs
+++ Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+// using System.Collections.Generic;
+// using User.SphinxRiddles;
 
-namespace riddles_of_the_sphinx
+namespace User
 {
-    class Program
+  public class Program
+  {
+    public static void Main()
     {
-        static void Main(string[] args)
+      Random rnd = new Random();
+      string[] riddles = { "What month of the year has 28 days?", "What is full of holes but still holds water?", "I shave every day, but my beard stays the same. What am I?", "I have branches, but no fruit, trunk or leaves. What am I?", "David’s parents have three sons: Snap, Crackle, and what’s the name of the third son?" };
+      string[] answers = { "all of them", "a sponge", "a barber", "a bank", "david" };
+
+      int randomRiddle()
+      {
+        return rnd.Next(riddles.Length);
+      }
+
+      for (int index = randomRiddle(); index < riddles.Length; randomRiddle())
+      {
+        Console.WriteLine(riddles[index]);
+        string userAnswer = Console.ReadLine();
+        if (userAnswer == answers[index])
         {
-            Console.WriteLine("Hello World!");
+          Console.WriteLine("That is correct.");
+          index = randomRiddle();
         }
+        else
+        {
+          Console.WriteLine("Sorry that was incorrect. Beat cheeks loser.");
+          break;
+        }
+      }
     }
+  }
 }
---
+++ sphinx.cs
@@ -0,0 +1,9 @@
+// namespace User.SphinxRiddles
+// {
+//   public class Riddles
+//   {
+//     public string[] riddles = { "What month of the year has 28 days?", "What is full of holes but still holds water?", "I shave every day, but my beard stays the same. What am I?", "I have branches, but no fruit, trunk or leaves. What am I?", "David’s parents have three sons: Snap, Crackle, and what’s the name of the third son?" };
+
+//     public string[] answers = { "All of them", "A sponge", "A barber", "A bank", "David" };
+//   }
+// }
